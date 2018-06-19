577648b86863a7f04762e03ca105c6fd319968b3
unknown <amarjit.bh88@gmail.com>
2018-06-19 09:54:13 +0530

    testing on openshift

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Amarjit Singh' });
 });
 
 module.exports = router;
